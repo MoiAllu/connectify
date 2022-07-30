c8b6a8045a8b046c3518a0d38e1f8036d673d145
Talal <talalstudent@gmail.com>
2022-07-30 23:07:23 +0500

    structure

---
+++ pages/settings/index.tsx
@@ -0,0 +1,11 @@
+import type { NextPage } from "next";
+
+const Settings: NextPage = () => {
+  return (
+    <div className="flex items-center justify-center h-screen bg-gray-50 px-4">
+      Settings
+    </div>
+  );
+};
+
+export default Settings;
