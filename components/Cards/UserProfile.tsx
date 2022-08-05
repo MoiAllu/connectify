2e01e622dbe00739ab2d3254257954792cfb8b2e
MoiAllu <al5690410@gmail.com>
2022-08-05 21:06:15 +0500

    side bar

--- components/Cards/UserProfile.tsx
+++ components/Cards/UserProfile.tsx
@@ -1,4 +1,18 @@
+import Image from "next/image";
 const UserProfile = () => {
-  return <div className="bg-white w-full h-full"> Ali abbasi </div>;
+  return (
+    <div className="bg-white w-full h-full">
+      <div className=" p-2   ">
+        <Image
+          className="rounded-xl "
+          width="820px"
+          height="312px"
+          alt="Photo"
+          src="/Background2.jpg"
+          objectFit="cover"
+        ></Image>
+      </div>
+    </div>
+  );
 };
 export default UserProfile;
